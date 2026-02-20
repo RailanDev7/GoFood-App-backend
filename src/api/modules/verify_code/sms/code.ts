99eceb201cb29bbf9a69d8affbcee5e1fa099585
Railan <railandejesusdossantos@gmail.com>
2026-02-19 22:00:43 -0300

    sms geneate code otp

--- src/api/modules/verify_code/sms/code.ts
+++ src/api/modules/verify_code/sms/code.ts
@@ -1,12 +1,34 @@
+import { Vonage } from "@vonage/server-sdk";
+import dotenv from 'dotenv';
+dotenv.config();
 
-export const otpCodeGenerate = async () => {
-    try {
-    return `${Math.floor(1000 + Math.random() * 9000)}`
-    } catch (error) {
-        
+const vonage = new Vonage({
+   apiKey: process.env.API_KEY as string,
+   apiSecret: process.env.API_SECRET as string,
+})
+
+export async function sendSMS(telefone: string): Promise<void> {
+  try {
+    const code: string = Math.floor(
+      1000 + Math.random() * 9000
+    ).toString();
+
+    const from: string = "GoFood";
+    const to: string = telefone;
+    const text: string = `GoFood código: ${code}. Válido nos próximos 15 minutos.`;
+
+    const response = await vonage.sms.send({
+      to,
+      from,
+      text,
+    });
+
+    console.log("Mensagem enviada com sucesso:", response);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error("Erro ao enviar SMS:", error.message);
+    } else {
+      console.error("Erro desconhecido:", error);
     }
+  }
 }
-
-const dados = otpCodeGenerate();
-
-console.log(dados)
